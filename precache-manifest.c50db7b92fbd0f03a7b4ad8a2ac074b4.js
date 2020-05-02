47b562c86332d2a05a0bdd66a05f01de6d813702
Steven Hsuante Wu <stevenwu@Stevens-MacBook-Air.local>
2020-05-02 00:12:46 -0700

    Updates

---
+++ precache-manifest.c50db7b92fbd0f03a7b4ad8a2ac074b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7f8af00cb68205257c125d26ddcae26",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0a103b3743e1a4a6cae9",
+    "url": "/monsters-rolodex/static/css/main.981652f4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a103b3743e1a4a6cae9",
+    "url": "/monsters-rolodex/static/js/main.5db48c54.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
